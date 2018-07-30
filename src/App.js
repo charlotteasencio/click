bfcef024015c595b25db4b5fda56627cf02bceb8
Charlotte Asencio <charlotte.e.asencio@gmail.com>
2018-07-29 21:45:18 -0400

    not functioning

--- src/App.js
+++ src/App.js
@@ -5,22 +5,82 @@ import Imagebox from './components/Imagebox/Imagebox';
 import images from "./images.json";
 import Imagegrid from './components/Imagegrid';
 
+
+function shuffleCards (array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+};
+
 class App extends Component {
 
   state = {
-    images
+    images,
+    currentScore: 0,
+    topScore: 0,
+    rightWrong: "",
+    clicked: [],
+  };
+
+  handleClick = id => {
+    if (this.state.clicked.indexOf(id) === -1) {
+      this.handleIncrement();
+      this.setState({ clicked: this.state.clicked.concat(id) });
+    } else {
+      this.handleReset();
+    }
+  };
+
+  handleIncrement = () => {
+    const newScore = this.state.currentScore + 1;
+    this.setState({
+      currentScore: newScore,
+      rightWrong: ""
+    });
+    if (newScore >= this.state.topScore) {
+      this.setState({ topScore: newScore });
+    }
+    else if (newScore === 12) {
+      this.setState({ rightWrong: "You win!" });
+    }
+    this.handleShuffle();
+  };
+
+  handleReset = () => {
+    this.setState({
+      currentScore: 0,
+      topScore: this.state.topScore,
+      rightWrong: "S",
+      clicked: []
+    });
+    this.handleShuffle();
+  };
+
+  handleShuffle = () => {
+    let shuffledCards = shuffleCards(images);
+    this.setState({ cards: shuffledCards });
   };
 
   render() {
     return (
     <div>
-      <Header/>
+      <Header
+        score={this.state.currentScore}
+        topScore={this.state.topScore}
+        rightWrong={this.state.rightWrong}
+      />
       <Imagegrid>
         {this.state.images.map(image => (
           <Imagebox
             id={image.id}
             key={image.id}
             image={image.image}
+            handleClick={this.handleClick}
+            handleIncrement={this.handleIncrement}
+            handleReset={this.handleReset}
+            handleShuffle={this.handleShuffle}
           />
         ))}
       </Imagegrid>
--- src/components/Imagebox/Imagebox.js
+++ src/components/Imagebox/Imagebox.js
@@ -3,7 +3,12 @@ import "./Imagebox.css";
 
 const Imagebox = props => (
 <div className="wrap">
-    <div className="card">
+    <div 
+        className="images"
+        value={props.id} 
+        onClick={() => props.handleClick(props.id)}
+    />
+    <div className="image">
         <img alt={props.id} src={props.image} />
     </div>
 </div>
