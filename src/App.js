ed1e679b015093536cc7243f5bbd1b446743f07d
Charlotte Asencio <34950490+charlotteasencio@users.noreply.github.com>
2018-07-31 08:55:50 -0400

    no images, no functionality, working on functions

--- src/App.js
+++ src/App.js
@@ -3,35 +3,42 @@ import "./App.css";
 import Header from "./components/Header";
 import Imagebox from "./components/Imagebox";
 import Imagegrid from "./components/Imagegrid";
+import Images from "./images.json";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      images: [],
+      Images,
+      clickedImages: [],
       score: 0,
-      topScore: 0,
-      clicked: false,
-      guessedCorrectly: null
+      topScore: 0
     };
 
     for (let i = 0; i < 10; i++) {
-      this.state.images.push(i);
+      this.state.Images.push(i);
     }
     console.log(this.state);
   }
 
   //function to handle click event
   //function to increment score
-  incrementScore = () => {
-    console.log("I've been clicked");
-    console.log(this.state);
-    this.setState({
-      score: this.state.score + 1,
-      clicked: true
-    });
+  imageClick = event => {
+    const currentImage = event.target.alt;
+    const imageAlreadyClicked =
+      this.state.clickedImages.indexOf(currentImage) > -1;
   };
 
+  if(imageAlreadyClicked) {
+    this.setState({
+      image: this.state.Image.sort(function(a, b) {
+        return 0.5 - Math.random();
+      }),
+      clickedImages: [],
+      score: 0
+    });
+    alert("You lose. Play again?");
+  }
   //function to shuffleCards
   //function for top score
   //reset function
@@ -54,7 +61,11 @@ class App extends Component {
           </p>
         </div>
         <Header />
-        <Imagegrid incrementScore={this.incrementScore} />
+        <Imagegrid>
+          {this.state.Images.map(image => (
+            <Imagebox id={image.id} key={image.id} image={image.image} />
+          ))}
+        </Imagegrid>
       </div>
     );
   }
